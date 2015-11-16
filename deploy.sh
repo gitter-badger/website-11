0226e7f6f11548f97c0602422fdd580a643089ee
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-11-16 18:09:20 +0800

    deploy: not always overwrite all git log
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,15 @@ set -e
 
 # Local variables
 OUT=".gh-pages"
+ID=$(git rev-parse --short HEAD)
+DATE=$(date)
 
 # clear and re-create the out directory
 rm -rf $OUT || exit 0;
-mkdir -p $OUT;
+git clone "https://${GH_TOKEN}@${GH_REF}" $OUT
+
+# remove all data in $OUT
+(cd $OUT; git rm -rf *)
 
 # Copy all prebuild files
 cp README.* $OUT
@@ -25,7 +30,7 @@ git config user.email "clojure.tw@gmail.com"
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
 git add .
-git commit -m "Deploy to GitHub Pages"
+git commit -m "Deploy commit $ID to GitHub Pages: $DATE"
 
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
